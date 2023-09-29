e40b003e416a1abddbf2f767f6b8fcbc7c70296c
developercyrus <developer.cyrus@gmail.com>
2023-09-29 11:49:14 +0800

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by cyrus on 29/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by cyrus on 29/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
